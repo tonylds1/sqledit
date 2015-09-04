3c0470ac067c22ab07c6480937b2cfd5c1561fea
valterjrdev <valterjrdev@gmail.com>
2015-09-04 20:13:16 -0300

    Adicionado as configurações de sessão

--- server/session.js
+++ server/session.js
@@ -6,7 +6,19 @@
      * @param {Object} app - instancia da app
      */
     module.exports = function (app) {
+        var session = require('express-session');
 
+        app.set('trust proxy', 1);
+        app.use(session({
+            secret: '11b120274fc8474aa312b5e46c473aa3',
+            resave: false,
+            saveUninitialized: true,
+            cookie: {
+                secure: true
+            }
+        }));
+
+        return app;
     }
 
 })();
