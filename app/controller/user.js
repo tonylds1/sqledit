6bd47c61cef2f1da1af54a999ae7de996bc6b9af
valterjrdev <valterjrdev@gmail.com>
2015-09-09 06:42:42 -0300

    Adicionado tela de aministração de usuários

--- app/controller/user.js
+++ app/controller/user.js
@@ -16,11 +16,152 @@
          * @return {Object} response
          */
         app.get('/user', function (request, response) {
-            return response.render('user/index');
+
+            app.models.user.find({}).sort({createdAt: 'desc' }).exec(function(error, model){
+                return response.render('user/index', {
+                    grid: model
+                });
+            });
+
         });
 
         /**
-         * Index
+         * View
+         *
+         * @param {Object} request
+         * @param {Object} response
+         * @return {Object} response
+         */
+        app.get('/user/view/:id', function (request, response) {
+
+            app.models.user.findOne({id: request.params.id}, function(error, model){
+                if(error){
+                    request.flash('danger', 'Erro ao tentar visualizar registro');
+                    return response.redirect('/user');
+                }
+
+                if(!model){
+                    request.flash('danger', 'Erro, registro inexistente');
+                    return response.redirect('/user');
+                }
+
+                return response.render('user/view', {
+                    model: model
+                });
+            });
+        });
+
+        /**
+         * Add
+         *
+         * @param {Object} request
+         * @param {Object} response
+         * @return {Object} response
+         */
+        app.get('/user/add', function (request, response) {
+            return response.render('user/add');
+        });
+
+        /**
+         * Create
+         *
+         * @param {Object} request
+         * @param {Object} response
+         * @return {Object} response
+         */
+        app.post('/user/create', function (request, response) {
+            app.models.user.create(request.body, function(error, model) {
+                if(error){
+                    request.flash('danger', 'Falha ao tentar realizar cadastro');
+                    return response.redirect('/user/add');
+                }
+
+                request.flash('success', 'Cadastro realizado com sucesso');
+                return response.redirect('/user/add');
+            });
+        });
+
+        /**
+         * Edit
+         *
+         * @param {Object} request
+         * @param {Object} response
+         * @return {Object} response
+         */
+        app.get('/user/edit/:id', function (request, response) {
+
+            app.models.user.findOne({id: request.params.id}, function(error, model){
+                if(error){
+                    request.flash('danger', 'Erro ao tentar visualizar registro');
+                    return response.redirect('/user');
+                }
+
+                if(!model){
+                    request.flash('danger', 'Erro, registro inexistente');
+                    return response.redirect('/user');
+                }
+
+                return response.render('user/edit', {
+                    model: model
+                });
+            });
+        });
+
+        /**
+         * Update
+         *
+         * @param {Object} request
+         * @param {Object} response
+         * @return {Object} response
+         */
+        app.post('/user/update', function (request, response) {
+            app.models.user.update({id: request.body.id}, request.body).exec(function (error, model){
+                if(error){
+                    request.flash('danger', 'Falha ao tentar atualizar registro');
+                    return response.redirect('/user/edit/' + request.body.id);
+                }
+
+                request.flash('success', 'Registro atualizado com sucesso');
+                return response.redirect('/user/edit/' + request.body.id);
+            });
+        });
+
+        /**
+         * Remove
+         *
+         * @param {Object} request
+         * @param {Object} response
+         * @return {Object} response
+         */
+        app.get('/user/remove/:id', function (request, response) {
+
+            app.models.user.findOne({id: request.params.id}, function(error, model){
+                if(error){
+                    request.flash('danger', 'Falha ao tentar remover registro');
+                    return response.redirect('/user');
+                }
+
+                if(!model){
+                    request.flash('danger', 'Erro, registro inexistente');
+                    return response.redirect('/user');
+                }
+
+                app.models.user.destroy({id: request.params.id}).exec(function(error, model) {
+                    if(error){
+                        request.flash('danger', 'Falha ao tentar remover registro');
+                        return response.redirect('/user');
+                    }
+
+                    request.flash('success', 'Registro removido com sucesso');
+                    return response.redirect('/user');
+                });
+            });
+        });
+
+
+
+        /**
+         * Signin
          *
          * @param {Object} request
          * @param {Object} response
@@ -90,7 +231,7 @@
          */
         app.post('/user/create', function (request, response) {
 
-            app.models.access.findOne({username: request.body.username}).exec(function findOneCB(error, model){
+            app.models.user.findOne({username: request.body.username}).exec(function findOneCB(error, model){
                 if(error){
                     request.flash('danger', 'Ocorreu uma falha no sistema');
                     return response.redirect('/');
